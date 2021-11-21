7017a77a272da20c81ab3f91c82666257818bda4
lavgup <lavyag01@gmail.com>
2021-11-21 16:02:31 +1100

    Use import instead

--- pages/api/domains.ts
+++ pages/api/domains.ts
@@ -1,15 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import fs from 'node:fs/promises';
-import path from 'node:path';
+import domains from '../../data/domains.json';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method !== 'GET') return res.status(405).json({
 		error: 'Method not allowed'
 	});
-
-	const file = await fs.readFile(path.join(process.cwd(), 'data', 'domains.json'));
-	const domains = JSON.parse(file.toString());
 
 	res.status(200).json(domains);
 }
