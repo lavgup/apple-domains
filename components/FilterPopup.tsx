6074b356b3fb5b73caf727d1d60881259c9dc2ee
lavgup <lavyag01@gmail.com>
2021-11-28 20:37:52 +1100

    Rename clear back to revert

--- components/FilterPopup.tsx
+++ components/FilterPopup.tsx
@@ -103,7 +103,7 @@ export default function FilterPopup({ domains }) {
 												excluded.forEach(include);
 											}}
 										>
-											Clear
+											Revert
 										</button>
 									</div>
 
